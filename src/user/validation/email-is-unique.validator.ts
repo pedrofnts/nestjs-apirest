6c208334b820e434112e3796003b4faa4f55d04a
Pedro Fontes <pedro7fontes@gmail.com>
2022-12-07 07:08:37 -0300

    feat: adds email validation

---
+++ src/user/validation/email-is-unique.validator.ts
@@ -0,0 +1,36 @@
+import { UserRepository } from './../user.repository';
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+@ValidatorConstraint({ async: true })
+export class EmailIsUniqueValidator implements ValidatorConstraintInterface {
+  constructor(private userRepository: UserRepository) {}
+  async validate(
+    value: any,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> {
+    const userWithEmailExists = await this.userRepository.existsWithEmail(
+      value,
+    );
+    return !userWithEmailExists;
+  }
+}
+
+export const EmailIsUnique = (validationOptions: ValidationOptions) => {
+  return (object: Object, property: string) => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: property,
+      options: validationOptions,
+      constraints: [],
+      validator: EmailIsUniqueValidator,
+    });
+  };
+};
